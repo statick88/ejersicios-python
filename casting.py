54a33d414fce7e088b9f51182455280a646cdccb
Diego Saavedra <dsaavedra88@gmail.com>
2026-04-20 21:46:02 -0500

    example casting str

--- casting.py
+++ casting.py
@@ -1,4 +1,10 @@
-texto_edad = "25"
+"""texto_edad = "25"
 print(type(texto_edad))
 edad = int(texto_edad)
-print(type(edad))
+print(type(edad))"""
+
+
+precio = 19.99
+print(type(precio))
+precio_texto = str(precio)
+print(type(precio_texto))
