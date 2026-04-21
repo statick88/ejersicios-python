aacec9a9ccb0c230bef85f14d30e0684096be786
Diego Saavedra <dsaavedra88@gmail.com>
2026-04-20 21:49:55 -0500

    example casting str and bool

--- casting.py
+++ casting.py
@@ -4,7 +4,12 @@ edad = int(texto_edad)
 print(type(edad))"""
 
 
-precio = 19.99
+"""precio = 19.99
 print(type(precio))
 precio_texto = str(precio)
-print(type(precio_texto))
+print(type(precio_texto))"""
+
+valor = 1
+print(type(valor))
+es_verdadero = bool(valor)
+print(type(es_verdadero))
