64d8e45e9ef459e4d2ff0386839a9cfd80e0e14a
Diego Saavedra <dsaavedra88@gmail.com>
2026-04-23 22:09:22 -0500

    while and reto

--- Estructuras_control/identacion_incorrecta.py
+++
@@ -1,9 +0,0 @@
-# ❌ IndentationError: Python usa indentación para definir bloques
-"""edad = 18
-if edad >= 18:
-print("Mayor de edad")  # Error: no está indentado"""
-
-# ✅ Correcto: 4 espacios de indentación
-edad = 18
-if edad >= 18:
-    print("Mayor de edad")  # 4 espacios desde el margen
